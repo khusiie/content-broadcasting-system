393c876eff5acac18f3fc0a35311e5f9104d8088
Khushi Gupta <kg7460502@gmail.com>
2026-04-28 02:13:53 +0530

    deploy link added

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(cors({
   origin: function (origin, callback) {
     if (!origin) return callback(null, true);
 
-    if (allowedOrigins.includes(origin)) {
+    if (allowedOrigins.includes(origin) || origin.endsWith('.vercel.app')) {
       return callback(null, true);
     } else {
       return callback(new Error("Not allowed by CORS"));
